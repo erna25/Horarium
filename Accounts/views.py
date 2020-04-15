9aaa5f5a4d92f9a1fdb27f10eacb2a6321a86ef4
erna25 <erna1999@yandex.ru>
2020-04-15 16:16:37 +0300

    Аутентификация

---
+++ Accounts/views.py
@@ -0,0 +1,4 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+#from django.contrib.auth.views import LoginView
+#from django.contrib.auth.views import LogoutView
